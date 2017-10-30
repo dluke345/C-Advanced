f3b496c7ec4c928c3c49bc8e1b7e6403f5add7a2
David Luke <dluke345@gmail.com>
2017-10-30 14:33:17 -0500

    Add files via upload

---
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            //Example 3:
+            var cheapBooks = books.FindAll(b => b.Price < 10);
+
+            foreach (var cheapBook in cheapBooks)
+            {
+                Console.WriteLine(cheapBook.Title);
+            }
+
+            // args => expression
+            //number => number * number;
+
+            // () => ...
+            // x => ...
+            // (x, y, x) => ...
+
+            //Example 2: 
+            //const int factor = 5;
+            //Func<int, int> multipler = n => n * factor;
+            //var result = multipler(10);
+            //Console.WriteLine(result);
+
+
+            //Example 1: 
+
+            //Func<int, int> square = number => number*number;
+            //Console.WriteLine(square(5));
+        }
+
+        //Don't need this method with Lambda Expression above
+        //static bool IsCheaperThan10Dollars(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
+
+        //Don't need this method with Lambda Expression above
+        //static int Square(int number)
+        //{
+        //    return number * number;
+        //}
+    }
+}
