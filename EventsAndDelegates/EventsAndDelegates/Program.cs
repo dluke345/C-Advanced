8b175037609819a6a6f4c20723370803c4cfbbdd
David Luke <dluke345@gmail.com>
2017-10-30 14:34:02 -0500

    Add files via upload

---
+++ EventsAndDelegates/EventsAndDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() {Title = "Video 1"};
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+            var messageService = new MessageService(); //another subscriber
+
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+}
---
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    //Publisher - this never needs to change, we can just add classes for different services.
+    public class VideoEncoder
+    {
+
+        //1 - Define a delegate, this is the long way. There is a built in EventHandler in C# we can use instead of creating our own delegate
+        ///public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        // EventHandler
+        // EventHandler<TEventArgs>
+
+        //2 - Define a event based on that delegate, this is how it would be if we created our own delegate
+        ///public event VideoEncodedEventHandler VideoEncoded;
+        
+        //If you wanted to publish an event without sending any additional data
+        ///public event EventHandler VideoEncoded;
+
+        //This is how it is when we use the built in EventHandler delegate
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        //3 - Raise the event
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+            }
+        }
+    }
+}
